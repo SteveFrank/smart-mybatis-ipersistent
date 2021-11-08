4383c95763513c883ec68c74e6eccda3ace4c255
FrankQian <yangqian3@staff.weibo.com>
2021-11-08 10:28:43 +0800

    init : 先编写测试模块

---
+++ smart-ipersistent-demo/src/main/java/com/smart/framework/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.smart.framework.demo.mapper;
+
+/**
+ * @author frankq
+ * @date 2021/11/8
+ */
+public interface UserMapper {
+
+
+
+}
---
+++ smart-ipersistent-demo/src/main/java/com/smart/framework/demo/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author frankq
+ * @date 2021/11/8
+ */
+package com.smart.framework.demo;
